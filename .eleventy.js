adeefe31a1aa3490ccb8b4ed2574773cc5996b3a
abhi <abhi@mailbox.org>
2022-02-16 11:05:28 +0100

    Remove rss plugin #8

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight'),
-    markdownIt = require('markdown-it'),
-    pluginRss = require('@11ty/eleventy-plugin-rss');
-
-const inspect = require("util").inspect;
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+// const pluginRss = require('@11ty/eleventy-plugin-rss');
 
 module.exports = (eleventyConfig) => {
     eleventyConfig.addPlugin(syntaxHighlight);
-    eleventyConfig.addPlugin(pluginRss);
+    //eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPassthroughCopy('assets/fonts');
     eleventyConfig.addPassthroughCopy('favicon.ico');
     const options = {
@@ -16,8 +14,6 @@ module.exports = (eleventyConfig) => {
     };
     eleventyConfig.setLibrary('md', markdownIt(options));
 
-    // custom filter to inspect data
-    eleventyConfig.addFilter("debug", (content) => `<pre>${inspect(content)}</pre>`);
 
     return {
         // Use liquid in html templates
